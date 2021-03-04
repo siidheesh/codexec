7e5f68245a2829d2bf78df076ac4a5782fa24038
siidheesh <siidheesh@gmail.com>
2021-03-04 12:26:27 +0800

    added comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use actix_web::{
+    http::StatusCode,
+    web::{self, Bytes},
+    App, HttpResponse, HttpServer,
+};
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
 use std::{
@@ -8,7 +13,7 @@ use std::{
     thread::{self, spawn},
     time::Duration,
 };
-//static cmd: &str = r#"ls && sleep 3 && ls"#; //r#"docker build -t pytemp /home/siid/projects/codexec/container/docker/ && docker run -it --rm --name pytemp_inst pytemp"#;
+
 static SCRIPT_NAME: &str = "script.py";
 static DOCKERFILE_NAME: &str = "Dockerfile";
 static DOCKERFILE: &str = r#"
@@ -28,18 +33,8 @@ pub enum Msg {
     Stdout(String),
     EOF,
     Error(String),
-    Timeout, //Stderr(String),
+    Timeout,
 }
-
-/*
-pub fn cleanup_jobs() {
-    Command::new("docker")
-        .args(&["rmi", "-f", &container_name])
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
-        .output()
-        .unwrap();
-}*/
 
 pub fn spawn_job(code: String, timeout_sec: u64) -> Receiver<Msg> {
     //Result<String, Job> {
@@ -56,19 +51,16 @@ pub fn spawn_job(code: String, timeout_sec: u64) -> Receiver<Msg> {
 
     let temp_dir = format!("/tmp/{}/", container_name);
 
+    // create temp dir
     fs::create_dir(&temp_dir).unwrap();
 
+    // create & write to the Dockerfile
     let mut dockerfile_path = temp_dir.clone();
     dockerfile_path.push_str(DOCKERFILE_NAME);
 
     fs::write(&dockerfile_path, DOCKERFILE).unwrap();
 
-    /*fs::copy(
-        "/home/siid/projects/codexec/container/docker/script.py",
-        &script_path,
-    )
-    .unwrap();*/
-
+    // create & write to SCRIPT_NAME.py
     let mut script_path = temp_dir.clone();
     script_path.push_str(SCRIPT_NAME);
 
@@ -77,6 +69,7 @@ pub fn spawn_job(code: String, timeout_sec: u64) -> Receiver<Msg> {
     script_file.flush().unwrap();
     drop(script_file); // idk if script_file is automatically dropped in time or not
 
+    // build the container
     Command::new("docker")
         .args(&["build", "-t", &container_name, &temp_dir])
         .stdout(Stdio::null())
@@ -84,10 +77,10 @@ pub fn spawn_job(code: String, timeout_sec: u64) -> Receiver<Msg> {
         .output()
         .unwrap();
 
-    //let mut lines = vec![];
     let (tx, rx) = channel();
     let tx_timer = tx.clone();
 
+    // run the container
     let mut output = Command::new("docker")
         .args(&["run", "-it", "--rm", &container_name])
         .stdout(Stdio::piped())
@@ -97,8 +90,8 @@ pub fn spawn_job(code: String, timeout_sec: u64) -> Receiver<Msg> {
 
     let stdout = output.stdout.take().unwrap();
 
+    // spawn a thread to read lines from container's stdout
     spawn(move || {
-        //output.stdout.as_mut() {
         let reader = BufReader::new(stdout);
         for line in reader.lines() {
             let _ = if let Ok(_line) = line {
@@ -117,44 +110,24 @@ pub fn spawn_job(code: String, timeout_sec: u64) -> Receiver<Msg> {
         }
     });
 
+    // spawn a thread to handle timeout
     spawn(move || {
         thread::sleep(Duration::from_secs(timeout_sec));
         let _ = tx_timer.send(Msg::Timeout);
     });
 
-    /*let receiver = spawn(move || -> Result<Vec<String>, Job> {
-        for recv in rx {
-            match recv {
-                Msg::Stdout(line) => lines.push(line),
-                Msg::EOF => return Ok(lines),
-                Msg::Error(msg) => return Err(Job::Error(msg)),
-                Msg::Timeout => return Err(Job::Timeout),
-            }
-        }
-        Ok(lines)
-    });
-
-    let res = match receiver.join().unwrap() {
-        Ok(lines) => Ok(lines.join("\n")),
-        Err(e) => Err(e),
-    };
-
-    cleanup_job(&temp_dir, &container_name);
-
-    res*/
-
+    // spawn a thread to intercept relay messages and handle cleanup
     let (tx1, rx1) = channel();
-
     spawn(move || {
-        for recv in rx {
-            match &recv {
+        for msg in rx {
+            match &msg {
                 Msg::EOF | Msg::Error(_) | Msg::Timeout => {
                     cleanup_job(&temp_dir, &container_name);
-                    let _ = tx1.send(recv);
+                    let _ = tx1.send(msg);
                     return; // drop tx1 and rx
                 }
                 _ => {
-                    let _ = tx1.send(recv);
+                    let _ = tx1.send(msg);
                 }
             }
         }
@@ -167,36 +140,29 @@ pub fn spawn_job(code: String, timeout_sec: u64) -> Receiver<Msg> {
 pub fn cleanup_job(temp_dir: &str, container_name: &str) {
     //println!("in cleanup_job: {}", container_name);
 
+    // forcibly remove container
     let _ = Command::new("docker")
         .args(&["rm", "-f", container_name])
         .stdout(Stdio::null())
         .stderr(Stdio::null())
         .spawn();
 
+    // forcibly remove image
     let _ = Command::new("docker")
         .args(&["rmi", "-f", container_name])
         .stdout(Stdio::null())
         .stderr(Stdio::null())
         .spawn();
 
+    // cleanup temp dir
     let _ = fs::remove_dir_all(temp_dir);
-}
-
-use actix_web::{
-    http::StatusCode,
-    web::{self, Bytes},
-    App, HttpRequest, HttpResponse, HttpServer, Responder,
-};
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
 }
 
 async fn handle_py(bytes: Bytes) -> Result<String, HttpResponse> {
     match String::from_utf8(bytes.to_vec()) {
         Ok(code) => {
             let mut lines = vec![];
+            //TODO: change to a ws impl
             for recv in spawn_job(code, EXEC_TIMEOUT) {
                 match recv {
                     Msg::Stdout(line) => lines.push(line),
@@ -216,6 +182,7 @@ async fn handle_py(bytes: Bytes) -> Result<String, HttpResponse> {
                     }
                 }
             }
+            // tx end was dropped, treat as success
             Ok(lines.join("\n"))
         }
         Err(_) => Err(HttpResponse::build(StatusCode::BAD_REQUEST).finish()),
@@ -224,13 +191,8 @@ async fn handle_py(bytes: Bytes) -> Result<String, HttpResponse> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-            .route("/py", web::post().to(handle_py))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/py", web::post().to(handle_py)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
